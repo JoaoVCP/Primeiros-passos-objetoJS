3c098e2f125d50e263dae7a52d642cbd507592aa
JoaoVCP <jvcallera@gmail.com>
2022-10-10 23:21:36 -0300

    Fim da primeira aula

--- desafio3.js
+++ desafio3.js
@@ -1,2 +1,19 @@
 //3) Adicionar informacoes importantes que ainda estao faltando, como telefone e tambem seus valores
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "17655455",
+    email: "andre@gmail.com"
+}
 
+console.log(cliente); //verificando que foi adicionado a chave telefone
+cliente.fone = "999354654";
+console.log(cliente);
+
+//alterando a chave telefone
+cliente.fone = "8755556455";
+console.log(cliente);
+
+//deletando
+delete cliente.fone;
+console.log(cliente.fone); //retornara undefined - da certo fazendo delete cliente["fone"];
