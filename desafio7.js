83ecc309a5144a893944d26e6d1011f6b3ec1f2d
JoaoVCP <jvcallera@gmail.com>
2022-10-10 23:57:41 -0300

    Aula 2

--- desafio7.js
+++ desafio7.js
@@ -1,2 +1,30 @@
 //ADicionar uma propriedade que permita "acoes", para que os clientes que estão cadastrados consigam fazer operacoes bancarias
 
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "17655455",
+    email: "andre@gmail.com",
+    fones: ["55999865465554","55654512455245"],
+    dependentes: [
+        {
+            nome:"Sara",
+            parentesco: "filha",
+            dataNasc: "23/03/2011"},
+        {
+            nome: "Samia Maria",
+            parentesco: "filha",
+            dataNasc: "04/01/2014"
+        }
+    ],
+    saldo: 100,
+
+    depositar: function(valor){
+        this.saldo += valor
+    }
+}
+
+console.log(cliente.saldo);
+cliente.depositar(30);
+console.log(cliente.saldo);
+
