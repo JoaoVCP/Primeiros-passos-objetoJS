955efdbedda51262447612169df0378b9d4bcf12
JoaoVCP <jvcallera@gmail.com>
2022-10-10 23:05:52 -0300

    Primeiras aulas

---
+++ desafio2.js
@@ -0,0 +1,3 @@
+//2) Consultar um objeto utilizando uma lista de chaves relativas a informacoes de clientes
+//gerada pelo sistema e printar o resultado na tela
+
---
+++ objetos.js
@@ -0,0 +1,12 @@
+//1 Desafio - Acessar um objeto com informações de um cliente e exibir essas informações no console
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "17655455",
+    email: "andre@gmail.com"
+}
+
+// console.log(cliente.nome);
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos.`); //usando template string
+console.log(cliente.cpf.substring(0, 3)); //comeca no 0 e para no indice 3
